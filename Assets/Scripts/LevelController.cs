85ae4022c296f3ee67b86a3fa88f6129668c86af
Enes Sarısaç <sarisacenes@gmail.com>
2021-12-13 19:20:05 +0300

    Create Main Menu(close #14)

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    public GameObject playButton,optionsButton,controlsButton,backButton,controlText;
+   public void GameStart()
+   {
+       SceneManager.LoadScene("Level1");
+   }
+   public void Options()
+   {
+       controlsButton.SetActive(true);
+       playButton.SetActive(false);
+       optionsButton.SetActive(false);
+       backButton.SetActive(true);
+        //SceneManager.LoadScene("");
+   }
+   public void BackButton()
+   { 
+       controlsButton.SetActive(false);
+       playButton.SetActive(true);
+       optionsButton.SetActive(true);
+       backButton.SetActive(false);
+       controlText.SetActive(false);
+
+   }
+   public void Controls()
+   {
+       controlsButton.SetActive(false);
+       controlText.SetActive(true);
+
+   }
+}
