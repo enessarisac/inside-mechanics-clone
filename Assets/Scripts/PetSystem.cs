5232a6d412f473779380ac07dbee923d4c04e3fe
itu-itis21-demirsu21 <33068096+CeyhunDemir@users.noreply.github.com>
2022-01-08 15:21:02 +0300

    Fix PetSystem and Add pet interactions

--- Assets/Scripts/PetSystem.cs
+++ Assets/Scripts/PetSystem.cs
@@ -9,40 +9,79 @@ public class PetSystem : MonoBehaviour
     public Vector3 maxOffset,minOffset;
     Vector3 velocity;
     public float smoothTime;
+    public bool inObj;
+    public GameObject selectedObj;
 
     float coroutineTimer=2f;
     void Start()
     {
-        
+        StartCoroutine(PetLocation());
+        StartCoroutine(GoIntoObjects());
+        StartCoroutine(offsetValue());
     }
     IEnumerator PetLocation()
     {
-        coroutineTimer=0;
-        offset.x=Random.Range(minOffset.x,maxOffset.x);
-        offset.y=Random.Range(minOffset.y,maxOffset.y);
-        offset.z=Random.Range(minOffset.z,maxOffset.z);
-        yield return new WaitForSeconds(2f);
-            
-        
+        while (true)
+        {
+            if (!inObj)
+            {
+                FollowPlayer();
+            }
+            yield return new WaitForSeconds(0);
+
+        }
+    }
+    IEnumerator GoIntoObjects()
+    {
+        while (true)
+        {
+            if (inObj)
+            {
+                transform.position = Vector3.SmoothDamp(transform.position, selectedObj.transform.position, ref velocity, smoothTime);
+            }
+            if (this.transform.position == selectedObj.transform.position)
+            {
+                yield return new WaitForSeconds(1);
+            }
+            else
+            {
+                yield return new WaitForSeconds(0.001f);
+            }
+        }
+    }
+    IEnumerator offsetValue()
+    {
+        while (true)
+        {
+            offset.x = Random.Range(minOffset.x, maxOffset.x);
+            offset.y = Random.Range(minOffset.y, maxOffset.y);
+            offset.z = Random.Range(minOffset.z, maxOffset.z);
+            yield return new WaitForSeconds(2f);
+        }
     }
     // Update is called once per frame
     void Update()
     {
-        if(coroutineTimer>1.9f){
-           StartCoroutine(PetLocation()); 
-         
-        }
-        else
+        if (Input.GetKeyDown(KeyCode.R))
         {
-            coroutineTimer+=1*Time.deltaTime;
+            inObj =!inObj;
+        }
+        if (inObj && Input.GetKeyDown(KeyCode.T))
+        {
+            selectedObj.GetComponent<Light>().enabled = !selectedObj.GetComponent<Light>().enabled;
         }
         
-        FollowPlayer();
+
     }
     void FollowPlayer()
     {
-
-        transform.position = Vector3.SmoothDamp(transform.position, player.transform.position+offset, ref velocity, smoothTime);
+        if (!inObj)
+            transform.position = Vector3.SmoothDamp(transform.position, player.transform.position+offset, ref velocity, smoothTime);
         
     }
+    void ControllingPet()
+    {
+
+    }
+    
 }
